ff114c8cfbb074dd2889d9d552495f848dd2c7f9
Vamsee Kanakala <vkanakala@gmail.com>
2011-03-15 14:39:37 +0530

    with accumulator

--- ex3_5.erl
+++ ex3_5.erl
@@ -24,16 +24,40 @@ concatenate([[]|T]) ->
 concatenate([[H|T]|T2]) ->
     [H|concatenate([T|T2])].
 
-flatten([H|[]]) ->
-    io:format("f1 - H: ~p~n", [H]),
-    flatten(H);
-flatten([[]|T]) ->
-    io:format("f2 - T: ~p~n", [T]),
-    flatten(T);
-flatten([[H|T]|T2]) ->
-    io:format("f3 - H: ~p T: ~p T2: ~p~n", [H, T, T2]),
-    %% concatenate([flatten([H|T]) | flatten(T2)]);
-    [flatten(H)|flatten(concatenate([T,T2]))];
-flatten(N) ->
-    io:format("f5 - called once~n"),
-    N.
+flatten(L)->
+    flatten_acc(L,[]).
+
+flatten_acc([], L) ->
+    io:format("f0~n"),
+    L;
+flatten_acc([H|[]],L) ->
+    io:format("f1 - H: ~p L:~p~n", [H, L]),
+    flatten_acc(H,L);
+flatten_acc([[]|T],L) ->
+    io:format("f2 - T: ~p L:~p~n", [T, L]),
+    flatten_acc(T,L);
+flatten_acc([H|T], L) ->
+    io:format("f3 - H: ~p T: ~p L: ~p~n", [H,T,L]),
+    flatten_acc(T, [H|L]);
+flatten_acc(N,L) ->
+    io:format("fN: ~p L~p~n", [N, L]),
+    flatten_acc(L, [N|L]).
+
+%% flatten_acc([[H|T]|T1],A) ->
+%%     io:format("f1 - H: ~p T: ~p T1: ~p A:~p~n", [H, T, T1, A]),
+%%     [flatten_acc(H,A)|[flatten_acc(T,A)|flatten_acc(T1,A)]];
+%% flatten_acc([H|[]],A) ->
+%%     io:format("f2 - H: ~p A:~p~n", [H, A]),
+%%     flatten_acc(H,A);
+%% flatten_acc([],A) ->
+%%     io:format("f0~p~n", [A]),
+%%     A;
+%% flatten_acc([[]],A) ->
+%%     io:format("f00~p~n", [A]),
+%%     A;
+%% flatten_acc(N,A) ->
+%%     io:format("fN: ~p A~p~n", [N, A]),
+%%     [N|A].
+
+
+
