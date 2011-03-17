4418114f3eaac12263533ca84f027420aea094ca
Vamsee Kanakala <vkanakala@gmail.com>
2011-03-17 14:22:06 +0530

    gave up, got solution from elsewhere

--- ex3_5.erl
+++ ex3_5.erl
@@ -1,6 +1,6 @@
 -module(ex3_5).
 -export([filter/2, reverse/1, 
-         concatenate/1, flatten/1]).
+         concatenate/1, flatten/1, is_list/1]).
 
 filter([], _) ->
     [];
@@ -24,36 +24,20 @@ concatenate([[]|T]) ->
 concatenate([[H|T]|T2]) ->
     [H|concatenate([T|T2])].
 
+is_list([_|_]) ->
+    true;
+is_list(_) ->
+    false.
+
+%% finally correct solution thanks to:
+%% http://benjaminhalsted.com/2010/03/erlang-programming-exercise-3-5-refactor/#comments
+
 flatten(L)->
     flatten_acc(L,[]).
 
 flatten_acc([], L) ->
-    io:format("f0: ~p~n", [L]),
     reverse(L);
-flatten_acc([[[]]|T], L) ->
-    io:format("f1: T: ~p L: ~p~n", [T, L]),
-    flatten_acc(T,L);
-flatten_acc([H|[]], L) ->
-    io:format("f3 - T: ~p L: ~p~n", [H, L]),
-    flatten_acc(H, L);
-flatten_acc([[]|T], L) ->
-    io:format("f2 - T: ~p L: ~p~n", [T, L]),
-    flatten_acc(T, L);
-flatten_acc([[[[H]]]|T], L) ->
-    io:format("f4 - H: ~p T: ~p L: ~p~n", [H,T,L]),
-    flatten_acc(T, [H|L]);
-flatten_acc([[[H]|T]|T1], L) ->
-    io:format("f5 - H: ~p T: ~p T1: ~p L: ~p~n", [H,T,T1,L]),
-    flatten_acc([T|T1], [H|L]);
-flatten_acc([[[H|T]]|T1], L) ->
-    io:format("f6 - H: ~p T: ~p T1: ~p L: ~p~n", [H,T,T1,L]),
-    flatten_acc([T|T1], [H|L]);
-flatten_acc([[H|T]|T1], L) ->
-    io:format("f7 - H: ~p T: ~p T1: ~p L: ~p~n", [H,T,T1,L]),
-    flatten_acc([T|T1], [H|L]);
+flatten_acc([H|T], L) when is_list(H) == true ->
+    flatten_acc(concatenate([H,T]), L);
 flatten_acc([H|T], L) ->
-    io:format("f8 - H: ~p T: ~p L: ~p~n", [H,T,L]),
-    flatten_acc(T, [H|L]);
-flatten_acc(N, L) ->
-    flatten_acc([], [N|L]).
-
+    flatten_acc(T, [H|L]).
