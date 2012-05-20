0495401faeca675e3cfef7e2e77198138a20fe2e
Vamsee Kanakala <vkanakala@gmail.com>
2012-05-20 19:53:00 +0530

    the rest of ex4

--- ch3/ex4.erl
+++ ch3/ex4.erl
@@ -1,5 +1,6 @@
 -module(ex4).
--export([new/0, destroy/1, write/3, read/2]).
+-export([new/0, destroy/1, write/3, 
+         read/2, match/2, delete/2]).
 
 new() ->
     [].
@@ -19,6 +20,30 @@ read(Key, [H|T]) ->
     end;
 read(_, []) ->
     {error, instance}.
+    
 
-        
+match(Val, Db) ->    
+    match_acc(Val, Db, []).
 
+match_acc(Val, [H|T], Matches) ->
+    {Key, _} = H,
+    case H == {Key, Val} of
+        true ->
+            match_acc(Val, T, [Key|Matches]);
+        false ->
+            match_acc(Val, T, Matches)
+    end;
+match_acc(_, [], Matches) ->
+    Matches.
+
+delete(Key, [H|T]) ->
+    {_, Val} = H,
+    case H == {Key, Val} of 
+        true ->
+            T;
+        false ->
+            [H|T]
+    end.
+    
+
+    
