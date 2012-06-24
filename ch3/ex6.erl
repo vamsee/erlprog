da0a4f5265349658c03e8c97fef70b2224cd9b60
Vamsee Kanakala <vkanakala@gmail.com>
2012-06-24 21:19:45 +0530

    Merge sort turning out to be a lot harder than I expected. I feel dumb.

--- ch3/ex6.erl
+++ ch3/ex6.erl
@@ -1,6 +1,6 @@
 -module(ex6).
 -import(ex5, [concatenate/1]).
--export([quicksort/1, split_list/1]).
+-export([quicksort/1, split_list/1, merge_sort/1, merge/3]).
 
 quicksort([]) ->
     [];
@@ -29,3 +29,21 @@ split_list_acc([H|T], Mid, Idx, Lft, Rgt) when Idx < Mid ->
     split_list_acc(T, Mid, Idx+1, [H|Lft], Rgt);
 split_list_acc([H|T], Mid, Idx, Lft, Rgt) when Idx >= Mid ->
     split_list_acc(T, Mid, Idx+1, Lft, [H|Rgt]).
+
+merge_sort(L) ->
+    [L1, L2] = split_list(L),
+    merge(L1, L2, []).
+
+merge([H1|[]], [H2|[]], _) ->
+    io:format("hit ~n"),
+    case H1 < H2 of 
+        true -> [H1, H2];
+        false -> [H2, H1]
+    end;
+merge(L1, L2, L) ->
+    io:format("L1: ~w~n", [L1]),
+    io:format("L2: ~w~n", [L2]),
+    io:format("L: ~w~n", [L]),
+    [L3, L4] = split_list(L1),
+    [L5, L6] = split_list(L2),
+    concatenate([merge(L3, L4, L), merge(L5, L6, L)]).
